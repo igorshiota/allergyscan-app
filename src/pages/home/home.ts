62779de29c87205a49d460e1d4dd751312a7f32f
Igor <igor.shiota@gmail.com>
2019-02-07 18:43:33 +0000

    A little debugging and added alert controllers

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,150 +1,188 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
-import { NavController, NavParams, ActionSheetController, LoadingController } from 'ionic-angular';
-import { Camera } from '@ionic-native/camera';
+import {
+  NavController,
+  NavParams,
+  ActionSheetController,
+  LoadingController,
+  AlertController
+} from "ionic-angular";
+import { Camera } from "@ionic-native/camera";
 
-import { Item } from '../../models/item.model';
-import items from '../../data/items';
+import { Item } from "../../models/item.model";
+import items from "../../data/items";
 
-import { ItemsService } from '../../services/items.service';
-
+import { ItemsService } from "../../services/items.service";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
-
 export class HomePage implements OnInit {
+  itemList: Item[];
+  founded = [];
+  imageUrl = "";
+  OCRAD: any;
 
-	itemList: Item[];
-	founded = [];
-	imageUrl = '';
-	OCRAD: any;
-	
-  constructor(public navCtrl: NavController,
-  			  private navParams: NavParams,
-  			  private itemsService: ItemsService,
-  			  private camera: Camera,
-  			  private actionSheetCtrl: ActionSheetController,
-  			  private loadingCtrl: LoadingController){}
+  constructor(
+    public navCtrl: NavController,
+    private navParams: NavParams,
+    private itemsService: ItemsService,
+    private camera: Camera,
+    private actionSheetCtrl: ActionSheetController,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
+  ) {}
 
-  ngOnInit(){
-  	//this.itemList = items;
-  	//console.log(this.itemList);
+  ngOnInit() {
+    //this.itemList = items;
+    //console.log(this.itemList);
   }
 
-  ionViewEnter(){
-  	this.loadItems();
+  ionViewEnter() {
+    this.loadItems();
   }
 
-  private loadItems(){
-  	this.itemList = this.itemsService.getItems();
+  private loadItems() {
+    this.itemList = this.itemsService.getItems();
   }
 
-  onAddItem(form: NgForm){
-  	this.itemsService.addItem(form.value.itemName);
-  	form.reset();
-  	this.loadItems();
+  onAddItem(form: NgForm) {
+    this.itemsService.addItem(form.value.itemName);
+    form.reset();
+    this.loadItems();
   }
 
-  onRemoveItem(index: number){
-  	this.itemsService.removeItem(index);
-  	this.loadItems();
+  onRemoveItem(index: number) {
+    this.itemsService.removeItem(index);
+    this.loadItems();
   }
 
-  onEmptyList(){
-  	this.itemsService.removeAllItems();
+  onEmptyList() {
+    this.itemsService.removeAllItems();
   }
-
 
   //Below is the code related to the camera/scan part
 
-  onShowActionSheet(){
-
-  	const actionSheet = this.actionSheetCtrl.create({
+  onShowActionSheet() {
+    const actionSheet = this.actionSheetCtrl.create({
       buttons: [
         {
-          text: 'Choose Photo',
+          text: "Choose Photo",
           handler: () => {
             this.onScan(0); // 0 == Library
           }
-        },{
-          text: 'Take Photo',
+        },
+        {
+          text: "Take Photo",
           handler: () => {
             this.onScan(1); // 1 == Camera
           }
-        },{
-          text: 'Demo Photo',
+        },
+        {
+          text: "Demo Photo",
           handler: () => {
-            this.imageUrl = 'assets/imgs/demo2.jpg';
+            this.imageUrl = "assets/imgs/demo2.jpg";
           }
-        },{
-          text: 'Cancel',
-          role: 'cancel'
+        },
+        {
+          text: "Cancel",
+          role: "cancel"
         }
       ]
     });
     actionSheet.present();
   }
 
-
-   onScan(sourceType: number) {
+  onScan(sourceType: number) {
     // You can check the values here:
     // https://github.com/driftyco/ionic-native/blob/master/src/plugins/camera.ts
-    this.camera.getPicture({
-      quality: 100,
-      destinationType: 0, // DATA_URL
-      sourceType,
-      allowEdit: true,
-      saveToPhotoAlbum: false,
-      correctOrientation: false
-    }).then((imageData) => {
-      this.imageUrl = `data:image/jpeg;base64,${imageData}`;
-    }, (err) => {
-      console.log(`ERROR -> ${JSON.stringify(err)}`);
+    this.camera
+      .getPicture({
+        quality: 100,
+        destinationType: 0, // DATA_URL
+        sourceType,
+        allowEdit: true,
+        saveToPhotoAlbum: false,
+        correctOrientation: false
+      })
+      .then(
+        imageData => {
+          this.imageUrl = `data:image/jpeg;base64,${imageData}`;
+        },
+        err => {
+          console.log(`ERROR -> ${JSON.stringify(err)}`);
+        }
+      );
+  }
+
+  onAnalyzeImage() {
+    let found = false;
+    let loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    loader.present();
+    (<any>window).OCRAD(document.getElementById("image"), text => {
+      loader.dismissAll();
+      let alert = this.alertCtrl.create({
+        title: "Label: ",
+        subTitle: text,
+        buttons: ["Dismiss"]
+      });
+      alert.present();
+      console.log(text);
+      for (const item of this.itemList) {
+        if (text.indexOf(item.name) >= 0) {
+          found = true;
+          break;
+        }
+      }
+
+      if (found) {
+        this.presentAlertNotSafe();
+      } else if (this.itemList.length <= 0) {
+        this.presentAlertAddItem();
+      } else {
+        this.presentAlertSafe();
+      }
     });
   }
 
-
-onAnalyzeImage() {
-	let found = false;
-    let loader = this.loadingCtrl.create({
-     content: 'Please wait...'
-    });
-    loader.present();
-    (<any>window).OCRAD(document.getElementById('image'), text => {  	  
-      loader.dismissAll();
-      alert(text);
-      console.log(text);
-	  	for(const item of this.itemList) 
-		  		{
-		  			if(text.indexOf(item.name) >=0){
-		  				found = true;
-		  				break;
-		  			}
-		  		}
-
-		  		if(found){
-		  			alert('This product is not safe!');
-		  		} else if(this.itemList.length <= 0){
-		  			alert('Please add an item to the list!');
-		  		} else{
-		  			alert('This product is safe!');
-		  		}
-				
-	      	}
-	      );
-	      
-	   }
-
-
   onRestart() {
-    this.imageUrl = '';
+    this.imageUrl = "";
     this.onShowActionSheet();
   }
 
+  // Alerts
 
+  presentAlertNotSafe() {
+    let alert = this.alertCtrl.create({
+      title: "Product not safe!",
+      subTitle: "This product contains the ingredient(s) listed!",
+      buttons: ["Dismiss"]
+    });
+    alert.present();
+  }
+
+  presentAlertSafe() {
+    let alert = this.alertCtrl.create({
+      title: "This Product is Safe!",
+      subTitle:
+        "Wohoo! This product does not contain any of the ingredients listed!",
+      buttons: ["Dismiss"]
+    });
+    alert.present();
+  }
+
+  presentAlertAddItem() {
+    let alert = this.alertCtrl.create({
+      title: "No Items were added!",
+      subTitle: "Please add an item to the list!",
+      buttons: ["Dismiss"]
+    });
+    alert.present();
+  }
 }
 /* INITIAL STUDY FUNCTION
 
